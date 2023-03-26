b46246b2d2a14185c4314dc133b87083607b3549
root <idrys01@gmail.com>
2023-03-26 09:41:30 -0700

    Limit format specification to 'c', 's' and '%', Other formats are treated without meaning and they are printed out as they appear in the string. e.g '%i' is printed without any meaning

--- _printf.c
+++ _printf.c
@@ -10,41 +10,42 @@
 int _printf(const char *format, ...)
 {
 	va_list arguments;
-	int size, index, char_count = 0;
+	int size, index = 0, char_count = 0;
 	bool flag = 0;
 	char pres_char, *arg_string, arg_char;
 
 	if (format == NULL)
 		return (char_count);
 	size = strlen(format);
-	index = 0;
 	va_start(arguments, format);
 	for (; index < size; index++)
 	{
 		pres_char = format[index];
 		if (flag) /* the character at format[index - 1] is '%' */
 		{
-			if (pres_char == 'c')
+			switch (pres_char)
 			{
-				arg_char = va_arg(arguments, int); /* char is promoted to int */
-				char_count += _print_char(&arg_char);
+				case 'c':
+					arg_char = va_arg(arguments, int); /* char is promoted to int */
+					char_count += _print_char(&arg_char);
+					break;
+				case 's':
+					arg_string = va_arg(arguments, char *);
+					char_count += _print_str(arg_string);
+					break;
+				case '%':
+					char_count += _print_char(format + index);
+					break;
+				default:
+					char_count += _print_char(format + (index - 1));
+					char_count += _print_char(format + index);
 			}
-			else if (pres_char == 's')
-			{
-				arg_string = va_arg(arguments, char *);
-				char_count += _print_str(arg_string);
-			}
-			else if (pres_char == '%')
-				char_count += _print_char(format + index);
 			flag = 0;
 		}
 		else if (pres_char == '%')
 			flag = 1;
 		else
-		{
-			write(1, format + index, 1); /* get address at index */
-			char_count++;
-		}
+			char_count += _print_char(format + index);
 	}
 	va_end(arguments); /* release the memory */
 	return (char_count);
