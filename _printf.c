663ef7afa09e8900e755b6a4d942cff644f58797
Dami Dunmade <dami@Damis-MacBook-Pro.local>
2023-03-27 21:03:27 +0100

    Added extra condition for NULL

--- _printf.c
+++ _printf.c
@@ -16,7 +16,7 @@ int _printf(const char *format, ...)
 
 	unsigned int len, i;
 
-	if (format == NULL)
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
 	va_start(arguments, format);
