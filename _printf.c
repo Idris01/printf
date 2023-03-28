9f6db877eed3cf1abdbaf2e4c483c834d1b5bf9e
Dami Dunmade <dami@Damis-MacBook-Pro.local>
2023-03-28 03:36:39 +0100

    Further formatting...

--- _printf.c
+++ _printf.c
@@ -36,13 +36,10 @@ int _printf(const char *format, ...)
 			i += 2;
 			continue;
 		}
-
 		if (!format[i + 1])
 			return (-1);
-
 		_putchar(format[i]);
 		len++;
-
 		if (format[i + 1] == '%')
 			i += 2;
 		else
